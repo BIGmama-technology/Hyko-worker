1031adaf68be6938ccee73daddb303d0a8240bd4
louai <lbo@big-mama.io>
2024-05-27 12:07:46 +0100

    feat: add system info and ping tasks

--- src/remote_worker.py
+++ src/remote_worker.py
@@ -1,6 +1,8 @@
 import os
+import platform
 from typing import Any
 
+import psutil
 from arq.connections import RedisSettings
 from hyko_sdk.models import StorageConfig
 from hyko_toolkit.registry import Registry
@@ -11,6 +13,22 @@ REDIS_SETTINGS = RedisSettings(
     password=os.getenv("REDIS_PASS"),
 )
 
+async def ping_worker(ctx: Any):
+    """Check if the worker is online."""
+    return "pong"
+
+
+async def get_system_info(ctx: Any):
+    """Arq task to get system info of worker."""
+    system_info = {
+        "operating_system": platform.system(),
+        "os_version": platform.version(),
+        "architecture": platform.machine(),
+        "cpu_cors": os.cpu_count(),
+        "total_memory": psutil.virtual_memory().total / (1024 ** 3)
+    }
+
+    return system_info
 
 async def execute_node(
     ctx: dict[str, Any],
@@ -44,6 +62,6 @@ class WorkerSettings:
     For a list of all available settings, see https://arq-docs.helpmanual.io/#arq.worker.Worker
     """
 
-    functions = [execute_node]
+    functions = [execute_node, get_system_info, ping_worker]
     redis_settings = REDIS_SETTINGS
     queue_name = os.getenv("QUEUE_NAME")
